825896108e610db1fba7d38bbdf9c196122b9357
zak-dev-art <zachndungu861@gmail.com>
2025-10-20 12:13:40 +0300

    modified files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Keypad from "./components/Keypad";
+import EyesOnMe from "./components/EyesOnMe";
+
+function App() {
+  return (
+    <div>
+      <h1>React Events Lab</h1>
+      <Keypad />
+      <EyesOnMe />
+    </div>
+  );
+}
+
+export default App;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
-// Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleChange() {
+    console.log("Entering password...");
+  }
+
+  return (
+    <div>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
